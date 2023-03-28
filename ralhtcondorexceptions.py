7acd0856488b8d8bc600f3020793f73c8fadf1c2
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2023-03-28 08:08:12 +0100

    clean up

--- ralhtcondorexceptions.py
+++ ralhtcondorexceptions.py
@@ -15,43 +15,9 @@ class ScheddNotReachable(Exception):
         return repr(self.value)
 
 
-class EmptySubmitFile(Exception):
-    def __init__(self):
-        self.value = "submit file is emtpy"
-    def __str__(self):
-        return repr(self.value)
-
-class MalformedSubmitFile(Exception):
-    def __init__(self, line):
-        self.value = 'line %s in submit file does not have the right format'
-    def __str__(self):
-        return repr(self.value)
-
-
 class IncorrectInputType(Exception):
     def __init__(self, name, type):
         self.value = 'Input option %s is not type %s' %(name, type)
-    def __str__(self):
-        return repr(self.value)
-
-
-class NegativeSubmissionNumber(Exception):
-    def __init__(self, value):
-        self.value = "Negative number of jobs to submit: %s" %value
-    def __str__(self):
-        return repr(self.value)
-
-
-class ErrorReadingSubmitFile(Exception):
-    def __init__(self, value):
-        self.value = "Unable to read the submit file %s" %value
-    def __str__(self):
-        return repr(self.value)
-
-
-class ErrorWritingSubmitFile(Exception):
-    def __init__(self, value):
-        self.value = "Unable to write the submit file %s" %value
     def __str__(self):
         return repr(self.value)
 
